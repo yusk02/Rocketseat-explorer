cd00bb3301ee98d77ade062b952290f443e7ccaa
Raphael Yusuke Nozaki <131203004+yusk02@users.noreply.github.com>
2023-10-25 22:31:41 -0300

    Add files via upload
    Revisão dos primeiros módulos!

---
+++ Stage 04 - Introdução à programação, lógica e algoritmos em Javascript/Projeto-Iniciando com JavaScript/main.js
@@ -0,0 +1,82 @@
+// Exercício 1
+
+alert("Hello World!")
+
+
+// E2
+
+let number1 = 10
+let number2 = 25
+
+let sum = alert(`A soma das variáveis declaradas no script é: ${number1 + number2}`)
+
+
+// E3 & E4 & E5
+
+let number = 1
+let string = true
+let boolean = "true"
+
+
+if(typeof number == "number") {
+  alert("A variável é um número!");
+} else {
+  alert("A variável não é um número!");
+}
+
+
+if(typeof string == "string") {
+  alert("A variável é uma string!");
+} else {
+  alert("A variável não é uma string!");
+}
+
+
+if(typeof boolean == "boolean") {
+  alert("A variável é um booleano!");
+} else {
+  alert("A variável não é um booleano!");
+}
+
+
+// E6
+
+let firstNumber = 52
+let secondNumber = 45
+
+alert(`O resultado da subtração " ${firstNumber} - ${secondNumber} " é igual a: ${firstNumber - secondNumber}`)
+
+
+// E7
+
+let n1 = 2
+let n2 = 13
+
+alert(`A multiplicação: " ${n1} x ${n2} " resulta em: ${n1 * n2}`)
+
+
+// E8
+
+let num1 = 84
+let num2 = 6
+
+alert(`A divisão de ${num1} por ${num2} tem como resultado: ${num1 / num2}`)
+
+
+// E9 & E10
+
+let even = 24
+let odd = 23
+
+
+if (even % 2 == 0) {
+  alert("O número da variável é par!")
+} else {
+  alert("O número da variável é ímpar!")
+}
+
+if (odd % 2 == 0) {
+  alert("O número da variável é par!")
+} else {
+  alert("O número da variável é ímpar!")
+}
