e637bd18657992e30447b0235c72d05ea5cac7ea
Raphael Yusuke Nozaki <131203004+yusk02@users.noreply.github.com>
2023-11-14 21:34:04 -0300

    Add files via upload

---
+++ Stage 05 - Avançando no JavaScript/Projeto09-calculadora-de-imc/js/script.js
@@ -0,0 +1,34 @@
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { notANumber, calculateIMC } from "./utils.js"
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = function(event) {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+  if(weightOrHeightIsNotANumber) {
+    AlertError.open()
+    return;
+  }
+
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
+  const message = `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
