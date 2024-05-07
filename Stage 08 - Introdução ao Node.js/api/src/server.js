031d22967004e0d0b6f06e696cf2f986ae102054
yusuke <ryusuken02@gmail.com>
2024-05-06 22:02:04 -0300

    add get method

--- Stage 08 - Introdução ao Node.js/api/src/server.js
+++ Stage 08 - Introdução ao Node.js/api/src/server.js
@@ -2,5 +2,14 @@ const express = require("express")
 
 const app = express()
 
+app.get("/message/:id/:user", (request, response) => {
+    const { id, user } = request.params
+
+    response.send(`
+    Id da mensagem: ${id}. 
+    Para o usuário: ${user}.
+    `)
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
