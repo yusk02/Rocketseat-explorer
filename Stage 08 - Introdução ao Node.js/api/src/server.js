1d2e8f7867e754b02d08f7145904ab72711245f7
yusuke <ryusuken02@gmail.com>
2024-05-08 23:28:24 -0300

    add post method

--- Stage 08 - Introdução ao Node.js/api/src/server.js
+++ Stage 08 - Introdução ao Node.js/api/src/server.js
@@ -1,20 +1,28 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params
 
-    response.send(`
-    Id da mensagem: ${id}. 
-    Nome do usuário: ${user}.
-    `)
-})
+// app.get("/message/:id/:user", (request, response) => {
+//     const { id, user } = request.params
 
-app.get("/users", (request, response) => {
-    const { page, limit } = request.query
+//     response.send(`
+//     Id da mensagem: ${id}. 
+//     Nome do usuário: ${user}.
+//     `)
+// })
 
-    response.send(`Página: ${page}. Mostrar: ${limit}`)
+// app.get("/users", (request, response) => {
+//     const { page, limit } = request.query
+
+//     response.send(`Página: ${page}. Mostrar: ${limit}`)
+// })
+
+app.post("/users", (request, response) => {
+    const { name, email, password} = request.body
+
+    response.json({ name, email, password})
 })
 
 const PORT = 3333
