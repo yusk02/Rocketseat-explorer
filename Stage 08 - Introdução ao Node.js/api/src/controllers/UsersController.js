54cb65f0f5f7a049b2a526db03f2daa528e8d6e5
yusuke <ryusuken02@gmail.com>
2024-06-11 16:00:34 -0300

    registering user/encrypting user password with bcryptjs

--- Stage 08 - Introdução ao Node.js/api/src/controllers/UsersController.js
+++ Stage 08 - Introdução ao Node.js/api/src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 const sqliteConnection = require("../database/sqlite")
@@ -14,11 +15,17 @@ class UsersController {
         const { name, email, password} = request.body
 
         const database = await sqliteConnection()
-        const checkUserExists = await database.get("SELECT * FROM users WHERE (?)", [email])
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
         if(checkUserExists) {
             throw new AppError("Este e-mail já está em uso.")
         }
+
+        const hashedPassword = await hash(password, 8)
+
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]
+        )
 
         return response.status(201).json()
     }
