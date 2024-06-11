edb38b8f7f3f07c9097b640b69b4441bce351c9a
yusuke <ryusuken02@gmail.com>
2024-06-11 17:22:36 -0300

    updating user name/email

--- Stage 08 - Introdução ao Node.js/api/src/controllers/UsersController.js
+++ Stage 08 - Introdução ao Node.js/api/src/controllers/UsersController.js
@@ -29,6 +29,36 @@ class UsersController {
 
         return response.status(201).json()
     }
+
+    async update(request,  response) {
+        const { name, email } = request.body
+        const { id } = request.params
+
+        const database = await sqliteConnection()
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado")
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Este email já está em uso.")
+        }
+
+        user.name = name
+        user.email = email
+
+        await database.run(`
+            UPDATE users SET 
+            name = ?,
+            email = ?,
+            updated_at = DATETIME('now')
+            WHERE id = ?`, [user.name, user.email, id])
+
+        return response.status(200).json()
+    }
 }
 
 module.exports = UsersController
