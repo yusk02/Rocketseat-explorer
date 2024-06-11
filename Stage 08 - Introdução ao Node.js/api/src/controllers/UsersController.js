67b05f08878a4b528d93c753f7d504ea98fb369e
yusuke <ryusuken02@gmail.com>
2024-06-11 17:38:57 -0300

    checking old password to update to a new password

--- Stage 08 - Introdução ao Node.js/api/src/controllers/UsersController.js
+++ Stage 08 - Introdução ao Node.js/api/src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 const sqliteConnection = require("../database/sqlite")
@@ -31,7 +31,7 @@ class UsersController {
     }
 
     async update(request,  response) {
-        const { name, email } = request.body
+        const { name, email, password, old_password } = request.body
         const { id } = request.params
 
         const database = await sqliteConnection()
@@ -50,12 +50,27 @@ class UsersController {
         user.name = name
         user.email = email
 
+        if(password && !old_password) {
+            throw new AppError("Informe a senha antiga para definir a nova senha")
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password)
+
+            if(!checkOldPassword) {
+                throw new AppError("Senha antiga incorreta.") 
+            }
+
+            user.password = await hash(password, 8)
+        }
+
         await database.run(`
             UPDATE users SET 
             name = ?,
             email = ?,
+            password = ?,
             updated_at = DATETIME('now')
-            WHERE id = ?`, [user.name, user.email, id])
+            WHERE id = ?`, [user.name, user.email, user.password, id])
 
         return response.status(200).json()
     }
