ad1a8632896e9d26c262f29a7846fd598cafbf6a
yusuke <ryusuken02@gmail.com>
2024-06-13 15:46:12 -0300

    correting code: if updated only password, keep the same user name and email

--- Stage 08 - Introdução ao Node.js/api/src/controllers/UsersController.js
+++ Stage 08 - Introdução ao Node.js/api/src/controllers/UsersController.js
@@ -47,8 +47,8 @@ class UsersController {
             throw new AppError("Este email já está em uso.")
         }
 
-        user.name = name
-        user.email = email
+        user.name = name ?? user.name
+        user.email = email ?? user.email
 
         if(password && !old_password) {
             throw new AppError("Informe a senha antiga para definir a nova senha")
