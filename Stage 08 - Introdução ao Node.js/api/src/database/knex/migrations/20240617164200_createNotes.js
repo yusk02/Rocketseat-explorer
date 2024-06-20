a3d0b48255cb7b2e38bfe81a30c199a2f00e0100
yusuke <ryusuken02@gmail.com>
2024-06-20 16:31:44 -0300

    add new schemas for 'tags' and 'links' tables

--- Stage 08 - Introdução ao Node.js/api/src/database/knex/migrations/20240617164200_createNotes.js
+++ Stage 08 - Introdução ao Node.js/api/src/database/knex/migrations/20240617164200_createNotes.js
@@ -1,4 +1,3 @@
-
 exports.up = knex => knex.schema.createTable("notes", table => {
     table.increments("id")
     table.text("title")
@@ -7,7 +6,6 @@ exports.up = knex => knex.schema.createTable("notes", table => {
 
     table.timestamp("created_at").default(knex.fn.now())
     table.timestamp("updated_at").default(knex.fn.now())
-
 })
 
 exports.down = knex => knex.schema.dropTable("notes")
