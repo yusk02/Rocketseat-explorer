3be425434f81b58baf4c6f47aa339edf34abb5eb
yusuke <ryusuken02@gmail.com>
2024-08-07 14:09:51 -0300

    creating 'users' table by migrations

--- Stage 08 - Introdução ao Node.js/app-node/db/knexfile.js
+++ Stage 08 - Introdução ao Node.js/app-node/db/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database.db'
     },
     useNullAsDefault: true,
     migrations: {
---
+++ Stage 08 - Introdução ao Node.js/app-node/db/migrations/20240807162618_create_users.js
@@ -0,0 +1,11 @@
+exports.up = knex => knex.schema.createTable("users", table => {
+  table.increments('id').primary()
+  table.string('name')
+  table.string('email')
+  table.string('password')
+  table.string('avatar')
+  table.timestamp('created_at').defaultTo(knex.fn.now())
+  table.timestamp('updated_at').defaultTo(knex.fn.now())
+})
+
+exports.down = knex => knex.schema.dropTable("users")
